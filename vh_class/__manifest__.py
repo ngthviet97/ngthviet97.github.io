cd85a971959e1ddb20d0bce084dfb356e0e5fcab
ngthviet97 <ngthviet97@gmail.com>
2020-06-25 16:53:21 +0700

    add vh_class

---
+++ vh_class/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Class',
+    'version': '1.0',
+    'category': '',
+    'sequence' : 5,
+    'summary':'',
+    'descrintion': "",
+    'website': 'https://www.thinkingerp.vn/',
+    'depens':[
+        'base',
+    ],
+    'data':[
+        'views/vh_class_view.xml',
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application':True,
+    'auto_install': False
+}
---
+++ vh_class/models/vh_class.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class VhClass(models.Model):
+    _name='vh.class'
+
+    name=fields.Char('Class Name')
+    soluong = fields.Integer()
+
+
+
+
+
+
