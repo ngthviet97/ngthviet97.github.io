4b5826a8634201fb9f3f8b0e934e629f463fb2b3
ngthviet97 <ngthviet97@gmail.com>
2020-06-27 03:55:34 +0700

    update

--- vh_class/models/vh_class.py
+++ vh_class/models/vh_class.py
@@ -1,9 +1,9 @@
 from odoo import models, fields
 
 class VhClass(models.Model):
-    _name='vh.class'
-
-    name=fields.Char('Class Name')
+ 
+    _name = 'vh.class'
+    name = fields.Char('Class Name')
     soluong = fields.Integer()
 
 
